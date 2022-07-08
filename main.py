3fe2a401eacfe6aedb53d6943d26fb54db92a37c
mjack257 <105824946+mjack257@users.noreply.github.com>
2022-07-08 09:42:04 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+from zoo import hours
+from zoo import hours as menagerie
+if __name__ == '__main__':
+    hours()
+    menagerie()
---
+++ zoo.py
@@ -0,0 +1,14 @@
+"""
+11.1 Create a file called zoo.py. In it, define a
+function called hours() that prints the string
+'Open 9-5 daily'. Then, use the interactive
+interpreter to import the zoo module and call its
+hours() function.
+
+11.2 In the interactive interpreter, import the zoo
+ module as menagerie and call its hours() function.
+
+"""
+
+def hours():
+    print("Open 9-5 daily")
